72bc6290208b2944a8fb8989d437d19f66b6fef8
Ivan <144273733+IvanTcanga@users.noreply.github.com>
2024-11-05 13:19:08 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -6,9 +6,3 @@ from helpers import *
 def create_courier():
     login_pass = register_new_courier_and_return_login_password()
     yield login_pass
-
-# @pytest.fixture()
-# def create_courier(courier_methods):
-#     response = courier_methods.create_courier(COURIER_NAME)
-#     yield response.json()['id']
-#     courier_methods.delete_courier(response.json()['id'])
