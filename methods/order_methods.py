54f5b0e823c26a2a666c41568be5d7231cff24d3
Ivan <144273733+IvanTcanga@users.noreply.github.com>
2024-11-05 13:24:59 +0300

    Update order_methods.py

--- methods/order_methods.py
+++ methods/order_methods.py
@@ -1,8 +1,18 @@
-from data import BASE_URL, ORDERS_URL
+import requests
+from data import *
+import allure
+import json
 
 
 class OrderMethods:
+    @allure.step("Создать заказ")
+    def post_orders(self, order_data):
+        order_data = json.dumps(order_data)
+        headers = {'Content-Type': 'application/json'}
+        r = requests.post(f'{URLs.BASE_URL}{URLs.CREATE_ORDERS_URL}', data=order_data, headers=headers)
+        return r
 
-    def post_order(self, id, params):
-        response = requests.post(f'{BASE_URL}{ORDERS_URL}finish/{id}', json=params)
-        return response.status_code, response.model_damp(mode='json')
+    @allure.step("Получение списка заказов")
+    def get_list_orders(self):
+        r = requests.get(f'{URLs.BASE_URL}{URLs.CREATE_ORDERS_URL}')
+        return r
